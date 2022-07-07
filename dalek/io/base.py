914da69f293d2b0f624c6371df8fc50f4ea39abe
Wolfgang Kerzendorf <wkerzendorf@gmail.com>
2022-07-07 16:27:51 -0400

    add new io

--- dalek/io/base.py
+++ dalek/io/base.py
@@ -1,35 +1,34 @@
 import os
 import h5py
 import pandas as pd
-
-
-
+import numpy as np
 class DalekDataSet():
-    
+
     @classmethod
     def from_hdf(cls, fname):
-        with pd.HDFStore(fname) as fh:
-            parameters = fh['parameters']
-            meta = fh['meta']
-            wavelength = fh['wavelength']
-        
-        with h5py.File(fname) as fh:
-            spectra = fh['spectra']
-        
-        return cls(meta, parameters, spectra, wavelength)
+        parameters = pd.read_hdf(fname, 'parameters')
+        meta = pd.read_hdf(fname, 'meta')
+        with h5py.File(fname, mode='r') as fh:
+            spectra = np.array(fh['spectra'])
+            wavelength = np.array(fh['wavelength'])
 
-    def __init__(self, meta, parameters, spectra, wavelength):
+        return cls(parameters, spectra, wavelength, meta)
+
+    def __init__(self, parameters, spectra, wavelength, meta=None):
         self.parameters = parameters
         self.spectra = spectra
         self.wavelength = wavelength
         self.meta = meta
         
-    def to_hdf(self, fname):
+    def to_hdf(self, fname, overwrite=False):
         if os.path.exists(fname):
-            raise IOError(f'Path {fname} exists')
+            if overwrite:
+                os.remove(fname)
+            else:
+                raise IOError(f'Path {fname} exists')
         
         with h5py.File(fname, mode='w') as fh:
-            fh['spectra'] = self.fluxes
+            fh['spectra'] = self.spectra
             fh['wavelength'] = self.wavelength
             
         self.parameters.to_hdf(fname, 'parameters')
