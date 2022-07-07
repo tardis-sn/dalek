6ebb691190351a650cce9b941aef9a035ce2d848
Wolfgang Kerzendorf <wkerzendorf@gmail.com>
2022-07-07 14:16:55 -0400

    new io_frame

--- dalek/io/base.py
+++ dalek/io/base.py
@@ -2,14 +2,25 @@ import os
 import h5py
 import pandas as pd
 
+
+
 class DalekDataSet():
+    
     @classmethod
     def from_hdf(cls, fname):
-        pass
+        with pd.HDFStore(fname) as fh:
+            parameters = fh['parameters']
+            meta = fh['meta']
+            wavelength = fh['wavelength']
+        
+        with h5py.File(fname) as fh:
+            spectra = fh['spectra']
+        
+        return cls(meta, parameters, spectra, wavelength)
 
-    def __init__(self, parameters, fluxes, wavelength, meta=None):
+    def __init__(self, meta, parameters, spectra, wavelength):
         self.parameters = parameters
-        self.fluxes = fluxes
+        self.spectra = spectra
         self.wavelength = wavelength
         self.meta = meta
         
@@ -18,7 +29,7 @@ class DalekDataSet():
             raise IOError(f'Path {fname} exists')
         
         with h5py.File(fname, mode='w') as fh:
-            fh['fluxes'] = self.fluxes
+            fh['spectra'] = self.fluxes
             fh['wavelength'] = self.wavelength
             
         self.parameters.to_hdf(fname, 'parameters')
